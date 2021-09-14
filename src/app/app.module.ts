9de13cb9bc325df9dbe3e4bbbf7c68243c20847c
bertismrpdl@gmail.com <bertismrpdl@gmail.com>
2021-09-14 14:49:40 -0700

    - changed app prefix - added custom styles - Implemented basic navbar - Implemented basic footer - Implemented rough design of the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_BASE_HREF} from "@angular/common";
 import { HttpClientModule} from "@angular/common/http";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
