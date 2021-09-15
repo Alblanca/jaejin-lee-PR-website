529a06b3c555e04af086afde2116630a67d0cebb
bertismrpdl@gmail.com <bertismrpdl@gmail.com>
2021-09-15 16:22:31 -0700

    Initial import of the backend Restructured the project

---
+++ backend/src/main/java/com/jaejinlee/jlwebsite/JlWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.jaejinlee.jlwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlWebsiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jaejinlee/jlwebsite/JlWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaejinlee.jlwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
